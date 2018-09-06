d4add6b10edb31f03e829c1b9c97bfbdd8e42c47
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-06 10:49:50 +0700

    29-XIR5-Jobsheet5

---
+++ src/Latihan1/NullPointerExpection.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class NullPointerExpection {
+    public static void main(String[] args){
+        String name="";
+        System.out.println("Length of the string " + name.length());
+    }
+}
---
+++ src/Latihan3/ArithmeticException.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan3;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+import java.util.Scanner;
+public class ArithmeticException {
+ public static void main(String args[])
+    {
+        //int a=100;
+        System.out.println("Enter the value for b");
+         Scanner console = new Scanner(System.in);
+	int b = console.nextInt();
+     
+        System.out.println("Enter the value for c");
+	int c = console.nextInt();
+        int res=10/(b-c);
+        System.out.println(" The result is "+res);  
+    }  
+}
